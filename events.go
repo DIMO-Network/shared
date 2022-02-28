96726ba76391ab997449c20f4d87010f45c7b028
Dylan Moreland <dylan@dimo.zone>
2022-02-28 14:27:50 -0500

    Add CloudEvent header struct

---
+++ events.go
@@ -0,0 +1,13 @@
+package shared
+
+import "time"
+
+// CloudEventHeaders contains the fields common to all CloudEvent messages.
+type CloudEventHeaders struct {
+	ID          string    `json:"id"`
+	Source      string    `json:"source"`
+	SpecVersion string    `json:"specversion"`
+	Subject     string    `json:"subject"`
+	Time        time.Time `json:"time"`
+	Type        string    `json:"type"`
+}
