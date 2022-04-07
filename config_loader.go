d706ea67a80c6e0269d226dfe0833f7f975ab25c
James Reategui <jreate@me.com>
2022-04-07 16:03:18 -0400

    add config loader with generics

--- config_loader.go
+++ config_loader.go
@@ -12,25 +12,26 @@ import (
 )
 
 // LoadConfig fills in all the values in the Settings from local yml file (for dev) and env vars (for deployments)
-func LoadConfig(filePath string) (*Settings, error) {
-	b, err := ioutil.ReadFile(filePath)
-	settings := Settings{}
+func LoadConfig[S any](filePath string) (S, error) {
+	file, err := ioutil.ReadFile(filePath)
+	var settings S
 	// if no file found, ignore as we could be running in higher level environment. We could make this more explicit with a cli parameter w/ the filename
 	if err != nil {
 		fmt.Printf("looks like running in higher level env as could not read file: %s \n", filePath)
 	} else {
-		settings, err = loadFromYaml(b)
+		settings, err = loadFromYaml[S](file)
 		if err != nil {
-			return nil, errors.Wrap(err, "could not load yaml")
+			return settings, errors.Wrap(err, "could not load yaml")
 		}
 	}
 	err = loadFromEnvVars(&settings) // override with any env vars found
 
-	return &settings, err
+	return settings, err
 }
 
-func loadFromYaml(yamlFile []byte) (Settings, error) {
-	var settings Settings
+// loadFromYaml loads yaml from text into object per type. Uses reflection to find yaml type metadata for lookup
+func loadFromYaml[S any](yamlFile []byte) (S, error) {
+	var settings S
 	err := yaml.Unmarshal(yamlFile, &settings)
 	if err != nil {
 		return settings, errors.Wrap(err, "could not unmarshall yaml file to settings")
@@ -38,9 +39,11 @@ func loadFromYaml(yamlFile []byte) (Settings, error) {
 	return settings, nil
 }
 
-func loadFromEnvVars(settings *Settings) error {
-	if settings == nil {
-		return errors.New("settings param cannot be nil")
+// loadFromEnvVars uses reflection to find yaml type metadata and lookup those values in environment variables for corresponding property.
+// settings must be passed in as a pointer
+func loadFromEnvVars[S any](settings S) error {
+	if reflect.ValueOf(settings).Kind() != reflect.Ptr {
+		return errors.New("settings must be passed in as a pointer, eg. &varname")
 	}
 	valueOfConfig := reflect.ValueOf(settings).Elem()
 	typeOfT := valueOfConfig.Type()
---
+++ config_loader_test.go
@@ -0,0 +1,51 @@
+package shared
+
+import (
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+)
+
+func Test_loadFromYaml(t *testing.T) {
+	var data = `
+PORT: 3000
+DB_CONNECT_STRING: mydb.aws.net
+ENV: dev
+`
+	settings, err := loadFromYaml[TestSettings]([]byte(data))
+	assert.NoError(t, err, "no error expected")
+	assert.NotNilf(t, settings, "settings not expected to be nil")
+	assert.Equal(t, 3000, settings.Port)
+	assert.Equal(t, "mydb.aws.net", settings.DbConnectString)
+	assert.Equal(t, "dev", settings.Env)
+}
+
+func Test_loadFromEnvVars(t *testing.T) {
+	settings := TestSettings{
+		Port:            3000,
+		DbConnectString: "mydb.aws.net",
+		Env:             "dev",
+	}
+	// these will now override the above
+	os.Setenv("ENV", "test")
+	os.Setenv("PORT", "5000")
+
+	err := loadFromEnvVars(&settings) // b/c of type inference we don't need to specify the type
+	assert.NoError(t, err)
+	assert.NotNilf(t, settings, "expected not nil")
+	assert.Equal(t, "test", settings.Env)
+	assert.Equal(t, 5000, settings.Port)
+	assert.Equal(t, "mydb.aws.net", settings.DbConnectString)
+}
+
+func Test_loadFromEnvVars_errIfNotPointer(t *testing.T) {
+	s := TestSettings{}
+	err := loadFromEnvVars(s)
+	assert.Error(t, err, "expected error if settings not a pointer")
+}
+
+type TestSettings struct {
+	Port            int    `yaml:"PORT"`
+	DbConnectString string `yaml:"DB_CONNECT_STRING"`
+	Env             string `yaml:"ENV"`
+}
