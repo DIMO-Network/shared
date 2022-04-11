0ff2e47b2f278696d74edbf5bb4d5f7708309938
James Reategui <JamesReate@users.noreply.github.com>
2022-04-11 09:39:16 -0400

    Http client wrapper (#4)
    * http client wrapper with tor but no retry
    
    * http client wrapper working

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/DIMO-Network/shared
 go 1.18
 
 require (
+	github.com/avast/retry-go/v4 v4.0.3
 	github.com/pkg/errors v0.9.1
 	github.com/stretchr/testify v1.7.0
 	google.golang.org/grpc v1.45.0
@@ -14,8 +15,8 @@ require (
 	github.com/davecgh/go-spew v1.1.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	golang.org/x/net v0.0.0-20200822124328-c89045814202 // indirect
-	golang.org/x/sys v0.0.0-20200323222414-85ca7c5b95cd // indirect
-	golang.org/x/text v0.3.0 // indirect
+	golang.org/x/net v0.0.0-20210805182204-aaa1db679c0d // indirect
+	golang.org/x/sys v0.0.0-20211107104306-e0b2ad06fe42 // indirect
+	golang.org/x/text v0.3.6 // indirect
 	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
 )
---
+++ http_client_wrapper.go
@@ -0,0 +1,84 @@
+package shared
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"time"
+
+	"github.com/avast/retry-go/v4"
+	"github.com/pkg/errors"
+)
+
+type HTTPClientWrapper interface {
+	ExecuteRequest(path, method string, body []byte) (*http.Response, error)
+}
+
+type httpClientWrapper struct {
+	httpClient  *http.Client
+	baseURL     string
+	headers     map[string]string
+	torProxyURL string
+}
+
+func NewHTTPClientWrapper(baseURL, torProxyURL string, timeoutSeconds time.Duration, headers map[string]string, addJSONHeaders bool) (HTTPClientWrapper, error) {
+	if addJSONHeaders {
+		headers["Accept"] = "application/json"
+		headers["Content-Type"] = "application/json"
+	}
+	client := &http.Client{
+		Timeout: timeoutSeconds * time.Second,
+	}
+	if torProxyURL != "" {
+		proxyURL, err := url.Parse(torProxyURL)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to parse Tor proxy URL")
+		}
+		client.Transport = &http.Transport{Proxy: http.ProxyURL(proxyURL)}
+	}
+	return &httpClientWrapper{
+		httpClient:  client,
+		baseURL:     baseURL,
+		headers:     headers,
+		torProxyURL: torProxyURL,
+	}, nil
+}
+
+// ExecuteRequest calls an endpoint, optional body and error handling. path is appended to the baseURL, same for json
+// headers and authorization. If request results in non 2xx response, will always return error with payload body in err message.
+// response should have defer response.Body.Close() after the error check as it could be nil when err is != nil
+func (h httpClientWrapper) ExecuteRequest(path, method string, body []byte) (*http.Response, error) {
+	req, err := http.NewRequest(method, h.baseURL+path, bytes.NewBuffer(body))
+	if err != nil {
+		return nil, err
+	}
+
+	if len(h.headers) > 0 {
+		for hk, hv := range h.headers {
+			req.Header.Set(hk, hv)
+		}
+	}
+	res := new(http.Response)
+
+	err = retry.Do(func() error {
+		res, err = h.httpClient.Do(req)
+		// handle error status codes
+		if err == nil && res != nil && res.StatusCode > 299 {
+			defer res.Body.Close() //nolint
+			body, err := ioutil.ReadAll(res.Body)
+			if err != nil {
+				return errors.Wrapf(err, "error reading failed request body")
+			}
+			errResp := errors.Errorf("received non success status code %d with body: %s", res.StatusCode, string(body))
+			if res.StatusCode == 400 || res.StatusCode == 401 {
+				// unrecoverable since probably bad payload
+				return retry.Unrecoverable(errResp)
+			}
+			return errResp
+		}
+		return nil
+	}, retry.Attempts(5), retry.Delay(500*time.Millisecond), retry.MaxDelay(9*time.Second))
+
+	return res, err
+}
