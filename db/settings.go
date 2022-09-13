aaf5b055f99410845f4de4217def16833e34b1c8
James Reategui <jreate@me.com>
2022-09-12 22:05:07 -0400

    fix settings names so make more sens in pkg

--- db/settings.go
+++ db/settings.go
@@ -4,26 +4,26 @@ import "fmt"
 
 // Settings connection settings to postgres db
 type Settings struct {
-	DBUser               string `yaml:"USER"`
-	DBPassword           string `yaml:"PASSWORD"`
-	DBPort               string `yaml:"PORT"`
-	DBHost               string `yaml:"HOST"`
-	DBName               string `yaml:"NAME"`
-	DBMaxOpenConnections int    `yaml:"MAX_OPEN_CONNECTIONS"`
-	DBMaxIdleConnections int    `yaml:"MAX_IDLE_CONNECTIONS"`
+	User               string `yaml:"USER"`
+	Password           string `yaml:"PASSWORD"`
+	Port               string `yaml:"PORT"`
+	Host               string `yaml:"HOST"`
+	Name               string `yaml:"NAME"`
+	MaxOpenConnections int    `yaml:"MAX_OPEN_CONNECTIONS"`
+	MaxIdleConnections int    `yaml:"MAX_IDLE_CONNECTIONS"`
 }
 
 // BuildConnectionString builds the connection string to the database - for now same as reader
 func (app *Settings) BuildConnectionString(withSearchPath bool) string {
 	cs := fmt.Sprintf("user=%s password=%s dbname=%s host=%s port=%s sslmode=disable",
-		app.DBUser,
-		app.DBPassword,
-		app.DBName,
-		app.DBHost,
-		app.DBPort,
+		app.User,
+		app.Password,
+		app.Name,
+		app.Host,
+		app.Port,
 	)
 	if withSearchPath {
-		cs = fmt.Sprintf("%s search_path=%s", cs, app.DBName) // assumption is schema has same name as dbname
+		cs = fmt.Sprintf("%s search_path=%s", cs, app.Name) // assumption is schema has same name as dbname
 	}
 	return cs
 }
