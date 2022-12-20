f2d331fcffd235fa3e4138bd549e6c99fc9fd438
Allyson-English <allyson.english@gmail.com>
2022-12-20 16:33:22 -0700

    move everything out of main into functions

--- scripts/main.go
+++ scripts/main.go
@@ -37,121 +37,56 @@ var dataTypes = map[string]string{
 	"double":   "float64",
 }
 
-func main() {
-
-	if len(os.Args) <= 1 {
-		log.Fatal(errors.New("yaml file required"))
-	}
-
-	arg := os.Args[1]
-
-	result := make(map[string]fields)
-
+func getDataFromFile(arg string) map[string]fields {
+	data := make(map[string]fields)
 	yamlFile, err := ioutil.ReadFile(arg)
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = yaml.Unmarshal(yamlFile, &result)
+	err = yaml.Unmarshal(yamlFile, &data)
 	if err != nil {
 		log.Fatal(err)
 	}
+	return data
+}
 
-	orderedKeys := make([]string, len(result))
+func writeToFile(fileName, data string) {
 
-	for k := range result {
-		orderedKeys = append(orderedKeys, k)
-	}
-
-	sort.Strings(orderedKeys)
-
-	currentSubStruct := ""
-	prevLen := 0
-	parentArray := []string{"Vehicle"}
-
-	headers := "package shared\n\nimport(\n\"fmt\"\n\"github.com/clarketm/json\"\n)\n\n"
-	dataStruct := "type DataSchemaStruct struct {\n"
-
-	for _, k := range orderedKeys {
-
-		fields := result[k]
-		attrs := strings.Split(k, ".")
-		currentSubStruct = attrs[len(attrs)-1]
-
-		if len(attrs) < prevLen {
-			diff := prevLen - len(attrs)
-			for i := 0; i < diff; i++ {
-				tag := makeJSONTag(parentArray[len(parentArray)-(i+1)])
-				dataStruct += fmt.Sprintf("} %s \n", tag)
-			}
-			parentArray = parentArray[:len(parentArray)-diff]
-		}
-
-		if parentArray[len(parentArray)-1] != currentSubStruct && fields.Type == "branch" {
-			parentArray = append(parentArray, currentSubStruct)
-		}
-
-		prevLen = len(attrs)
-		switch fields.Type {
-		case "branch":
-			currentSubStruct = attrs[len(attrs)-1]
-			dataStruct += "// " + fields.Description
-			dataStruct += "\n" + currentSubStruct + " struct {" + "\n"
-
-		case "attribute", "sensor", "actuator":
-			description := "// " + fields.Description
-			varAndType := "\n" + currentSubStruct + " " + dataTypes[fields.DataType]
-			if len(fields.Allowed) != 0 {
-				description += fmt.Sprintf(" Must be one of [\"%s\"]", strings.Join(fields.Allowed, `", "`))
-			}
-			tag := makeJSONTag(currentSubStruct)
-			dataStruct += description + varAndType + tag + "\n"
-
-		default:
-			if fields.Type != "" {
-				log.Println("unrecognized type: ", fields)
-			}
-		}
-	}
-
-	if parentArray != nil {
-		tag := makeJSONTag(parentArray[0])
-		dataStruct += fmt.Sprintf("\n} %s }", tag)
-	}
-
-	methods := `
-	func NewVehicleStatus() DataSchemaStruct {
-		return DataSchemaStruct{}
-	}
-
-	func (d DataSchemaStruct) Marshal() ([]byte, error) {
-		if valid, err := d.IsValid(); !valid {
-			return []byte{}, err
-		}
-
-		return json.Marshal(d)
-	}
-	`
-
-	f, err := os.Create("data_schema_struct.go")
+	f, err := os.Create(fileName)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer f.Close()
+	_, err = f.WriteString(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = exec.Command("go", "fmt", fileName).Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
-	validChecks := validEnumCheck(orderedKeys, result)
+func main() {
+	if len(os.Args) <= 1 {
+		log.Fatal(errors.New("yaml file required"))
+	}
+	arg := os.Args[1]
 
+	data := getDataFromFile(arg)
+
+	orderedKeys := make([]string, len(data))
+	for k := range data {
+		orderedKeys = append(orderedKeys, k)
+	}
+	sort.Strings(orderedKeys)
+
+	headers := "package shared\n\nimport(\n\"fmt\"\n\"github.com/clarketm/json\"\n)\n\n"
+	dataStruct := generateStruct(orderedKeys, data)
+	validChecks := validEnumCheck(orderedKeys, data)
+	methods := generateMethods()
 	finalOutput := headers + validChecks + dataStruct + methods
-
-	_, err = f.WriteString(finalOutput)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	_, err = exec.Command("go", "fmt", "data_schema_struct.go").Output()
-	if err != nil {
-		log.Fatal(err)
-	}
-
+	writeToFile("data_schema_struct.go", finalOutput)
 }
 
 func makeJSONTag(varName string) string {
@@ -166,18 +101,7 @@ func lowerFirstLetter(s string) string {
 }
 
 func validEnumCheck(data []string, orderedKeys map[string]fields) string {
-	isvalidchecks := `
-	func contains(s []string, str string) bool {
-		for _, v := range s {
-			if v == str {
-				return true
-			}
-		}
-		return false
-	}
-	
-	func(d DataSchemaStruct) IsValid() (bool, error){
-		`
+	isvalidchecks := `func(d DataSchemaStruct) IsValid() (bool, error){`
 
 	n := 0
 	for _, kvalidate := range data {
@@ -215,6 +139,86 @@ func validEnumCheck(data []string, orderedKeys map[string]fields) string {
 	}
 
 	isvalidchecks += "\nreturn true, nil}\n\n"
-
 	return isvalidchecks
+}
+
+func generateMethods() string {
+	methods := `
+	func NewVehicleStatus() DataSchemaStruct {
+		return DataSchemaStruct{}
+	}
+
+	func (d DataSchemaStruct) Marshal() ([]byte, error) {
+		if valid, err := d.IsValid(); !valid {
+			return []byte{}, err
+		}
+
+		return json.Marshal(d)
+	}
+
+	func contains(s []string, str string) bool {
+		for _, v := range s {
+			if v == str {
+				return true
+			}
+		}
+		return false
+	}
+	`
+
+	return methods
+}
+
+func generateStruct(orderedKeys []string, data map[string]fields) string {
+
+	currentSubStruct := ""
+	prevLen := 0
+	parentArray := []string{"Vehicle"}
+	dataStruct := "type DataSchemaStruct struct {\n"
+
+	for _, k := range orderedKeys {
+		fields := data[k]
+		attrs := strings.Split(k, ".")
+		currentSubStruct = attrs[len(attrs)-1]
+
+		if len(attrs) < prevLen {
+			diff := prevLen - len(attrs)
+			for i := 0; i < diff; i++ {
+				tag := makeJSONTag(parentArray[len(parentArray)-(i+1)])
+				dataStruct += fmt.Sprintf("} %s \n", tag)
+			}
+			parentArray = parentArray[:len(parentArray)-diff]
+		}
+		if parentArray[len(parentArray)-1] != currentSubStruct && fields.Type == "branch" {
+			parentArray = append(parentArray, currentSubStruct)
+		}
+		prevLen = len(attrs)
+		switch fields.Type {
+		case "branch":
+			currentSubStruct = attrs[len(attrs)-1]
+			dataStruct += "// " + fields.Description
+			dataStruct += "\n" + currentSubStruct + " struct {" + "\n"
+		case "attribute", "sensor", "actuator":
+			description := "// " + fields.Description
+			varAndType := "\n" + currentSubStruct + " " + dataTypes[fields.DataType]
+			if len(fields.Allowed) != 0 {
+				description += fmt.Sprintf(" Must be one of [\"%s\"]", strings.Join(fields.Allowed, `", "`))
+			}
+			tag := makeJSONTag(currentSubStruct)
+			dataStruct += description + varAndType + tag + "\n"
+
+		default:
+			if fields.Type != "" {
+				log.Println("unrecognized type: ", fields)
+			}
+		}
+	}
+
+	if parentArray != nil {
+		tag := makeJSONTag(parentArray[0])
+		dataStruct += fmt.Sprintf("\n} %s }", tag)
+	}
+
+	return dataStruct
+
 }
