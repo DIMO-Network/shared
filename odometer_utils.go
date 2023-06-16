980ffc0dd49dc5d48ef2418df094a98d4f222b29
James Reategui <JamesReate@users.noreply.github.com>
2023-06-16 14:56:20 -0400

    additional odo check filter (#37)

--- odometer_utils.go
+++ odometer_utils.go
@@ -5,7 +5,7 @@ func IsOdometerValid(odometer float64) bool {
 	if odometer <= 100 {
 		return false
 	}
-	if odometer == 65539 || odometer == 65538 {
+	if odometer == 65539 || odometer == 65538 || odometer == 983040 {
 		return false
 	}
 	return true
