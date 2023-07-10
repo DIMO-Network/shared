daacfc2d6d8d4ccb999da51ae1bcf056c0cb962a
Allyson-English <allyson.english@gmail.com>
2023-07-10 13:34:55 -0600

    add fingerprint data and signature to headers

--- events.go
+++ events.go
@@ -11,3 +11,13 @@ type CloudEventHeaders struct {
 	Time        time.Time `json:"time"`
 	Type        string    `json:"type"`
 }
+
+type FingerprintData struct {
+	RpiUptimeSecs  int      `json:"rpiUptimeSecs,omitempty"`
+	BatteryVoltage float64  `json:"batteryVoltage,omitempty"`
+	Timestamp      int64    `json:"timestamp"`
+	Vin            string   `json:"vin"`
+	Protocol       string   `json:"protocol"`
+	Odometer       float64  `json:"odometer,omitempty"`
+	Errors         []string `json:"errors,omitempty"`
+}
