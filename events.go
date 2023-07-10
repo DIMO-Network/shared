b3020f17164725afdbcf0b9cad63b2ba2c32ed58
Allyson-English <allyson.english@gmail.com>
2023-07-10 13:30:43 -0600

    didn't mean to delete headers

--- events.go
+++ events.go
@@ -1,5 +1,17 @@
 package shared
 
+import "time"
+
+// CloudEventHeaders contains the fields common to all CloudEvent messages.
+type CloudEventHeaders struct {
+	ID          string    `json:"id"`
+	Source      string    `json:"source"`
+	SpecVersion string    `json:"specversion"`
+	Subject     string    `json:"subject"`
+	Time        time.Time `json:"time"`
+	Type        string    `json:"type"`
+}
+
 type FingerprintData struct {
 	RpiUptimeSecs  int      `json:"rpiUptimeSecs,omitempty"`
 	BatteryVoltage float64  `json:"batteryVoltage,omitempty"`
