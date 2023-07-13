45c2edacce98e8aad1b6d91f1cbf85f0b8b9083e
Dylan Moreland <dylan@dimo.zone>
2023-07-13 18:42:00 -0400

    Generic Kafka consumer, based on Sarama

--- go.mod
+++ go.mod
@@ -15,7 +15,7 @@ require (
 	github.com/lib/pq v1.10.7
 	github.com/pkg/errors v0.9.1
 	github.com/rs/zerolog v1.28.0
-	github.com/stretchr/testify v1.7.2
+	github.com/stretchr/testify v1.8.1
 	golang.org/x/exp v0.0.0-20230105000112-eab7a2c85304
 	google.golang.org/grpc v1.45.0
 	google.golang.org/protobuf v1.28.0
@@ -23,6 +23,26 @@ require (
 )
 
 require (
+	github.com/eapache/go-resiliency v1.3.0 // indirect
+	github.com/eapache/go-xerial-snappy v0.0.0-20230111030713-bf00bc1b83b6 // indirect
+	github.com/eapache/queue v1.1.0 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/hashicorp/errwrap v1.0.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/hashicorp/go-uuid v1.0.3 // indirect
+	github.com/jcmturner/aescts/v2 v2.0.0 // indirect
+	github.com/jcmturner/dnsutils/v2 v2.0.0 // indirect
+	github.com/jcmturner/gofork v1.7.6 // indirect
+	github.com/jcmturner/gokrb5/v8 v8.4.3 // indirect
+	github.com/jcmturner/rpc/v2 v2.0.3 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/pierrec/lz4/v4 v4.1.17 // indirect
+	github.com/rcrowley/go-metrics v0.0.0-20201227073835-cf1acfcdf475 // indirect
+	github.com/rogpeppe/go-internal v1.11.0 // indirect
+)
+
+require (
+	github.com/Shopify/sarama v1.38.1
 	github.com/andybalholm/brotli v1.0.5 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.12 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.6 // indirect
---
+++ kafka/group.go
@@ -0,0 +1,68 @@
+package kafka
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/Shopify/sarama"
+	"github.com/rs/zerolog"
+)
+
+type Config struct {
+	Brokers []string
+	Topic   string
+	Group   string
+}
+
+type wrap[A any] struct {
+	handler func(A) error
+	logger  *zerolog.Logger
+}
+
+func (w *wrap[A]) Setup(sarama.ConsumerGroupSession) error { return nil }
+
+func (w *wrap[A]) Cleanup(sarama.ConsumerGroupSession) error { return nil }
+
+func (w *wrap[A]) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
+	for {
+		select {
+		case msg := <-claim.Messages():
+			var a A
+			if err := json.Unmarshal(msg.Value, &a); err != nil {
+				w.logger.Err(err).Msg("Failed unmarshaling message.")
+				continue
+			}
+			if err := w.handler(a); err != nil {
+				w.logger.Err(err).Msg("Error processing message.")
+			}
+		case <-session.Context().Done():
+			return nil
+		}
+	}
+}
+
+func Consume[A any](ctx context.Context, config Config, handler func(A) error, logger *zerolog.Logger) error {
+	g, err := sarama.NewConsumerGroup(config.Brokers, config.Group, nil)
+	if err != nil {
+		return err
+	}
+
+	w := wrap[A]{handler: handler, logger: logger}
+
+	go func() {
+		for {
+			if err := g.Consume(ctx, []string{config.Topic}, &w); err != nil {
+				logger.Err(err).Msg("Consumer group session ended with an error.")
+			}
+			if ctx.Err() != nil {
+				logger.Info().Msg("Context canceled, shutting down.")
+				if err := g.Close(); err != nil {
+					logger.Err(err).Msg("Error closing consumer group.")
+				}
+				return
+			}
+		}
+	}()
+
+	return nil
+}
