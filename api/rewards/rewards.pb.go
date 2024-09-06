3d25a4d4d59f86f684292cafb83dec0b170fce7a
Dylan Moreland <dylan@dimo.zone>
2024-09-06 13:52:18 -0400

    Note and creation time

--- api/rewards/rewards.pb.go
+++ api/rewards/rewards.pb.go
@@ -467,7 +467,9 @@ type GetBlacklistStatusResponse struct {
 	sizeCache     protoimpl.SizeCache
 	unknownFields protoimpl.UnknownFields
 
-	IsBlacklisted bool `protobuf:"varint,1,opt,name=is_blacklisted,json=isBlacklisted,proto3" json:"is_blacklisted,omitempty"`
+	IsBlacklisted bool                   `protobuf:"varint,1,opt,name=is_blacklisted,json=isBlacklisted,proto3" json:"is_blacklisted,omitempty"`
+	Note          string                 `protobuf:"bytes,2,opt,name=note,proto3" json:"note,omitempty"`
+	CreatedAt     *timestamppb.Timestamp `protobuf:"bytes,3,opt,name=created_at,json=createdAt,proto3" json:"created_at,omitempty"`
 }
 
 func (x *GetBlacklistStatusResponse) Reset() {
@@ -509,6 +511,20 @@ func (x *GetBlacklistStatusResponse) GetIsBlacklisted() bool {
 	return false
 }
 
+func (x *GetBlacklistStatusResponse) GetNote() string {
+	if x != nil {
+		return x.Note
+	}
+	return ""
+}
+
+func (x *GetBlacklistStatusResponse) GetCreatedAt() *timestamppb.Timestamp {
+	if x != nil {
+		return x.CreatedAt
+	}
+	return nil
+}
+
 type SetBlacklistStatusRequest struct {
 	state         protoimpl.MessageState
 	sizeCache     protoimpl.SizeCache
@@ -517,7 +533,7 @@ type SetBlacklistStatusRequest struct {
 	EthereumAddress []byte `protobuf:"bytes,1,opt,name=ethereum_address,json=ethereumAddress,proto3" json:"ethereum_address,omitempty"`
 	IsBlacklisted   bool   `protobuf:"varint,2,opt,name=is_blacklisted,json=isBlacklisted,proto3" json:"is_blacklisted,omitempty"`
 	// A note describing why someone was blacklisted, typically containing the name of the
-	// person doing it and a link to an issue.
+	// person doing it and a link to an issue. Ignored if is_blacklisted is false.
 	Note string `protobuf:"bytes,3,opt,name=note,proto3" json:"note,omitempty"`
 }
 
@@ -669,12 +685,17 @@ var file_api_rewards_rewards_proto_rawDesc = []byte{
 	0x6c, 0x69, 0x73, 0x74, 0x53, 0x74, 0x61, 0x74, 0x75, 0x73, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73,
 	0x74, 0x12, 0x29, 0x0a, 0x10, 0x65, 0x74, 0x68, 0x65, 0x72, 0x65, 0x75, 0x6d, 0x5f, 0x61, 0x64,
 	0x64, 0x72, 0x65, 0x73, 0x73, 0x18, 0x01, 0x20, 0x01, 0x28, 0x0c, 0x52, 0x0f, 0x65, 0x74, 0x68,
-	0x65, 0x72, 0x65, 0x75, 0x6d, 0x41, 0x64, 0x64, 0x72, 0x65, 0x73, 0x73, 0x22, 0x43, 0x0a, 0x1a,
-	0x47, 0x65, 0x74, 0x42, 0x6c, 0x61, 0x63, 0x6b, 0x6c, 0x69, 0x73, 0x74, 0x53, 0x74, 0x61, 0x74,
-	0x75, 0x73, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x12, 0x25, 0x0a, 0x0e, 0x69, 0x73,
-	0x5f, 0x62, 0x6c, 0x61, 0x63, 0x6b, 0x6c, 0x69, 0x73, 0x74, 0x65, 0x64, 0x18, 0x01, 0x20, 0x01,
-	0x28, 0x08, 0x52, 0x0d, 0x69, 0x73, 0x42, 0x6c, 0x61, 0x63, 0x6b, 0x6c, 0x69, 0x73, 0x74, 0x65,
-	0x64, 0x22, 0x81, 0x01, 0x0a, 0x19, 0x53, 0x65, 0x74, 0x42, 0x6c, 0x61, 0x63, 0x6b, 0x6c, 0x69,
+	0x65, 0x72, 0x65, 0x75, 0x6d, 0x41, 0x64, 0x64, 0x72, 0x65, 0x73, 0x73, 0x22, 0x92, 0x01, 0x0a,
+	0x1a, 0x47, 0x65, 0x74, 0x42, 0x6c, 0x61, 0x63, 0x6b, 0x6c, 0x69, 0x73, 0x74, 0x53, 0x74, 0x61,
+	0x74, 0x75, 0x73, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x12, 0x25, 0x0a, 0x0e, 0x69,
+	0x73, 0x5f, 0x62, 0x6c, 0x61, 0x63, 0x6b, 0x6c, 0x69, 0x73, 0x74, 0x65, 0x64, 0x18, 0x01, 0x20,
+	0x01, 0x28, 0x08, 0x52, 0x0d, 0x69, 0x73, 0x42, 0x6c, 0x61, 0x63, 0x6b, 0x6c, 0x69, 0x73, 0x74,
+	0x65, 0x64, 0x12, 0x12, 0x0a, 0x04, 0x6e, 0x6f, 0x74, 0x65, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09,
+	0x52, 0x04, 0x6e, 0x6f, 0x74, 0x65, 0x12, 0x39, 0x0a, 0x0a, 0x63, 0x72, 0x65, 0x61, 0x74, 0x65,
+	0x64, 0x5f, 0x61, 0x74, 0x18, 0x03, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x1a, 0x2e, 0x67, 0x6f, 0x6f,
+	0x67, 0x6c, 0x65, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62, 0x75, 0x66, 0x2e, 0x54, 0x69, 0x6d,
+	0x65, 0x73, 0x74, 0x61, 0x6d, 0x70, 0x52, 0x09, 0x63, 0x72, 0x65, 0x61, 0x74, 0x65, 0x64, 0x41,
+	0x74, 0x22, 0x81, 0x01, 0x0a, 0x19, 0x53, 0x65, 0x74, 0x42, 0x6c, 0x61, 0x63, 0x6b, 0x6c, 0x69,
 	0x73, 0x74, 0x53, 0x74, 0x61, 0x74, 0x75, 0x73, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x12,
 	0x29, 0x0a, 0x10, 0x65, 0x74, 0x68, 0x65, 0x72, 0x65, 0x75, 0x6d, 0x5f, 0x61, 0x64, 0x64, 0x72,
 	0x65, 0x73, 0x73, 0x18, 0x01, 0x20, 0x01, 0x28, 0x0c, 0x52, 0x0f, 0x65, 0x74, 0x68, 0x65, 0x72,
@@ -756,23 +777,24 @@ var file_api_rewards_rewards_proto_depIdxs = []int32{
 	11, // 0: rewards.GetQualifiedDevicesRequest.start:type_name -> google.protobuf.Timestamp
 	11, // 1: rewards.GetQualifiedDevicesRequest.end:type_name -> google.protobuf.Timestamp
 	5,  // 2: rewards.GetDeviceRewardsResponse.weeks:type_name -> rewards.DeviceRewardsWeek
-	12, // 3: rewards.RewardsService.GetTotalPoints:input_type -> google.protobuf.Empty
-	12, // 4: rewards.RewardsService.GetAverageTokens:input_type -> google.protobuf.Empty
-	2,  // 5: rewards.RewardsService.GetQualifiedDevices:input_type -> rewards.GetQualifiedDevicesRequest
-	4,  // 6: rewards.RewardsService.GetDeviceRewards:input_type -> rewards.GetDeviceRewardsRequest
-	7,  // 7: rewards.RewardsService.GetBlacklistStatus:input_type -> rewards.GetBlacklistStatusRequest
-	9,  // 8: rewards.RewardsService.SetBlacklistStatus:input_type -> rewards.SetBlacklistStatusRequest
-	0,  // 9: rewards.RewardsService.GetTotalPoints:output_type -> rewards.GetTotalPointsResponse
-	1,  // 10: rewards.RewardsService.GetAverageTokens:output_type -> rewards.AverageTokensResponse
-	3,  // 11: rewards.RewardsService.GetQualifiedDevices:output_type -> rewards.GetQualifiedDevicesDevice
-	6,  // 12: rewards.RewardsService.GetDeviceRewards:output_type -> rewards.GetDeviceRewardsResponse
-	8,  // 13: rewards.RewardsService.GetBlacklistStatus:output_type -> rewards.GetBlacklistStatusResponse
-	10, // 14: rewards.RewardsService.SetBlacklistStatus:output_type -> rewards.SetBlacklistStatusResponse
-	9,  // [9:15] is the sub-list for method output_type
-	3,  // [3:9] is the sub-list for method input_type
-	3,  // [3:3] is the sub-list for extension type_name
-	3,  // [3:3] is the sub-list for extension extendee
-	0,  // [0:3] is the sub-list for field type_name
+	11, // 3: rewards.GetBlacklistStatusResponse.created_at:type_name -> google.protobuf.Timestamp
+	12, // 4: rewards.RewardsService.GetTotalPoints:input_type -> google.protobuf.Empty
+	12, // 5: rewards.RewardsService.GetAverageTokens:input_type -> google.protobuf.Empty
+	2,  // 6: rewards.RewardsService.GetQualifiedDevices:input_type -> rewards.GetQualifiedDevicesRequest
+	4,  // 7: rewards.RewardsService.GetDeviceRewards:input_type -> rewards.GetDeviceRewardsRequest
+	7,  // 8: rewards.RewardsService.GetBlacklistStatus:input_type -> rewards.GetBlacklistStatusRequest
+	9,  // 9: rewards.RewardsService.SetBlacklistStatus:input_type -> rewards.SetBlacklistStatusRequest
+	0,  // 10: rewards.RewardsService.GetTotalPoints:output_type -> rewards.GetTotalPointsResponse
+	1,  // 11: rewards.RewardsService.GetAverageTokens:output_type -> rewards.AverageTokensResponse
+	3,  // 12: rewards.RewardsService.GetQualifiedDevices:output_type -> rewards.GetQualifiedDevicesDevice
+	6,  // 13: rewards.RewardsService.GetDeviceRewards:output_type -> rewards.GetDeviceRewardsResponse
+	8,  // 14: rewards.RewardsService.GetBlacklistStatus:output_type -> rewards.GetBlacklistStatusResponse
+	10, // 15: rewards.RewardsService.SetBlacklistStatus:output_type -> rewards.SetBlacklistStatusResponse
+	10, // [10:16] is the sub-list for method output_type
+	4,  // [4:10] is the sub-list for method input_type
+	4,  // [4:4] is the sub-list for extension type_name
+	4,  // [4:4] is the sub-list for extension extendee
+	0,  // [0:4] is the sub-list for field type_name
 }
 
 func init() { file_api_rewards_rewards_proto_init() }
